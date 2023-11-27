a189f4c4ab35286b9b08739e290f074d05d911bd
naveenterance <naveenterance@gmail.com>
2023-11-27 05:33:49 +0530

    working in node

---
+++ index.js
@@ -0,0 +1,40 @@
+const redux = require("redux");
+const createStore = redux.createStore;
+
+const CAKE_ORDERED = "CAKE_ORDERED";
+
+function orderCake() {
+  return {
+    type: CAKE_ORDERED,
+    quantity: 1,
+  };
+}
+
+const initialState = {
+  numofCakes: 10,
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case CAKE_ORDERED:
+      return {
+        ...state,
+        numofCakes: state.numofCakes - 1,
+      };
+    default:
+      return state;
+  }
+};
+
+const store = createStore(reducer);
+console.log("Initial state", store.getState());
+
+const unsubscribe = store.subscribe(() =>
+  console.log("Update state", store.getState())
+);
+
+store.dispatch(orderCake());
+store.dispatch(orderCake());
+store.dispatch(orderCake());
+
+unsubscribe();
